be0134b998878196edbead81d6bfc050d42ad0bc
Andrew Yu <andrewthomasyu@gmail.com>
2021-01-10 15:27:00 +0800

    Add password input functionality to message creation

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,16 +1,16 @@
 from flask import request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, BooleanField
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, PasswordField
 from wtforms.validators import ValidationError, DataRequired
 from app.models import User
 
 class MessageForm(FlaskForm):
     Message = TextAreaField('Message', render_kw={"rows": 11, "cols": 60})
     Recipient = StringField('Recepient')
-    Password = StringField('Password')
+    Password = PasswordField('Password')
     submit = SubmitField('Send')
 
 class PasskeyForm(FlaskForm):
-    passkey = StringField('Password')
+    passkey = PasswordField('Password')
     submit = SubmitField('Open')
 
