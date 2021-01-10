7f931aad066978a667fe851b02cad5feb88f5910
Andrew Yu <andrewthomasyu@gmail.com>
2021-01-10 12:29:50 +0800

    Edits to logger error messages

--- app/__init__.py
+++ app/__init__.py
@@ -38,14 +38,14 @@ def create_app(config_class=Config):
             mail_handler = SMTPHandler(
                 mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
                 fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-                toaddrs=app.config['ADMINS'], subject='Kickoff Failure',
+                toaddrs=app.config['ADMINS'], subject='iGreet Failure',
                 credentials=auth, secure=secure)
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
 
         if not os.path.exists('logs'):
             os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/kickoff.log',
+        file_handler = RotatingFileHandler('logs/igreet.log',
                                            maxBytes=10240, backupCount=10)
         file_handler.setFormatter(logging.Formatter(
             '%(asctime)s %(levelname)s: %(message)s '
@@ -54,7 +54,7 @@ def create_app(config_class=Config):
         app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
-        app.logger.info('Kickoff startup')
+        app.logger.info('iGreet startup')
 
     return app
 
