7910611eb076c1df71cc66052f4581e54fd51a3e
Andrew Yu <andrewthomasyu@gmail.com>
2021-01-10 14:43:09 +0800

    Add initial API route for viewing message view page

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request, g, current_app
 from flask_login import current_user, login_required
 from app import db
-from app.main.forms import MessageForm
+from app.main.forms import MessageForm, PasskeyForm
 from app.models import User, Message
 from app.main import bp
 
@@ -21,10 +21,21 @@ def index():
     
     return render_template('index.html', form=form)
 
-@bp.route('/display/<message_id>/<recipient_hash>')
-def display_message(message_id, recipient_hash):
-    message = Message.query.get(int(message_id))
-    message_body = message.body
-    recipient = message.restore_recipient_name()
+@bp.route('/display/<message_id>', methods={'GET','POST'})
+def display_message(message_id):
+    #message = Message.query.get(int(message_id))
+    #message_body = message.body
+    #recipient = message.restore_recipient_name()
+    form = PasskeyForm()
 
-    return render_template('message.html')
+    if form.validate_on_submit():
+        passkey = form.passkey.data
+        #validated = message.check_passkey(passkey)
+        validated = True
+
+        if validated:
+            return render_template('message.html', validated=True, name='Linte', message_body='placeholder')
+        else:
+            return render_template('message.html', form=form, validated=False, name='Linte')
+
+    return render_template('message.html', form=form, validated=False, name='Linte')
