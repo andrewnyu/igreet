794f4965b90fe6192855043c0dc1a64821cc9f7f
Andrew Yu <andrewthomasyu@gmail.com>
2021-03-07 15:50:08 +0800

    Add feature on url masking to improve privacy and improve stability on db id creation

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,29 +5,37 @@ from app import db
 from app.main.forms import MessageForm, PasskeyForm
 from app.models import User, Message
 from app.main import bp
+from sqlalchemy import func
 
 @bp.route('/', methods={'GET','POST'})
 @bp.route('/index', methods={'GET','POST'})
 def index():
     form = MessageForm()
     if form.validate_on_submit():
-        message = Message(body=form.Message.data, recipient=form.Recipient.data)
+        import string
+        import random
+        url_mask = random.choice(string.ascii_letters)+random.choice(string.ascii_letters)
+        message = Message(body=form.Message.data, recipient=form.Recipient.data, url_mask = url_mask)
         message.set_passkey(form.Password.data)
 
         db.session.add(message)
         db.session.commit()
 
-        id = Message.query.count()
-        link = "igreet.live/display/"+str(id)
+        id = db.session.query(func.max(Message.id)).scalar()
+        link = "igreet.live/display/"+str(id)+"/"+url_mask
         flash("Your message has been posted!")
         return render_template('submission.html', link=link)
     
     return render_template('index.html', form=form)
 
-@bp.route('/display/<message_id>', methods={'GET','POST'})
-def display_message(message_id):
+@bp.route('/display/<message_id>', methods={'GET','POST'}, defaults={'url_mask':None})
+@bp.route('/display/<message_id>/<url_mask>', methods={'GET','POST'})
+def display_message(message_id,url_mask):
+    #Split url argument to take relevant parts
+
     message = Message.query.get(int(message_id))
     message_body = message.body
+    message_url_mask = message.url_mask
     recipient = message.recipient
     form = PasskeyForm()
 
@@ -44,4 +52,7 @@ def display_message(message_id):
         else:
             return render_template('message.html', form=form, validated=False, name=recipient)
 
-    return render_template('message.html', form=form, validated=False, name=recipient)
+    if url_mask == message_url_mask:
+        return render_template('message.html', form=form, validated=False, name=recipient)
+    else:
+        return render_template('404.html')
